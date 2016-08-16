0915bf379e2266a261b369e7c0b241f236db4205
teeppope <terraepop@gmail.com>
2016-08-15 22:15:23 -0700

    tweak background image on front-page

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -21,6 +21,7 @@
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 		<header id="masthead" class="site-header" role="banner">
+			<!-- <div class="header-bump"></div> -->
 			<div class="container">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
