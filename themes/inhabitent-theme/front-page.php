85035e247def76881ecd3ccf3930a1685e396af1
teeppope <terraepop@gmail.com>
2016-08-16 13:42:35 -0700

    setup base shop category get_terms loop

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -28,27 +28,26 @@ get_header(); ?>
 
 			<ul class="product-card-list">
 				<?php 
-				$taxonomy = 'product_types';
-				// $term_args= array(
-				// 		  'hide_empty' => false,
-				// 		  'orderby' => 'name',
-				// 		  'order' => 'ASC'
-				// 		  );
-				$args = array(
-    					'taxonomy' => 'tax_name',
-    					'description' => 'description',
+				$categories = array(
+					'taxonomy' => 'product-type', 
+					'hide_empty' => true,
 					);
-				// $front_page_blog_posts = get_posts($args);
-				$front_page_product_terms = get_terms( $taxonomy, $args );
 
-					?>
+				$terms = get_terms( $categories );
+				print_r($terms);
+
+				?>
 				
-				<?php foreach ( $front_page_product_terms as $product_term ) : setup_postdata( $terms ); ?>
-				   <li> 
-
-					<!-- Make Button for read more -->
-					<button><a href="<?php the_permalink(); ?>"> <?php single_term_title( 'taxonomy' );?> </a></button>
-				   </li>
+				<?php foreach ( $terms as $term ) : setup_postdata( $term ); ?>
+					<li> 
+						
+						<h3><?php echo $term->name ?></h3>
+						<p><?php echo $term->description?></p>
+						<!-- Make Button for read more -->
+						<button><a href="<?php the_permalink(); ?>"> <?php single_term_title( 'taxonomy' );?> 
+							<?php echo $term->name . ' Stuff'?>
+						</a></button>
+					</li>
 
 				<?php endforeach; wp_reset_postdata(); ?>
 			</ul>
@@ -69,26 +68,26 @@ get_header(); ?>
 					'posts_per_page' => 3,
 					);
 				$front_page_blog_posts = get_posts($args);
-					?>
+				?>
 				
 				<?php foreach ( $front_page_blog_posts as $post ) : setup_postdata( $post ); ?>
-				   <li> 
-				  <!--  // Gets the post thumbnail MIGHT NEED TO WRAP EACH part in divs well see -->
+					<li> 
+						<!--  // Gets the post thumbnail MIGHT NEED TO WRAP EACH part in divs well see -->
 
-					 <?php if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail( 'medium' ); ?>
-					<?php endif; ?>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'medium' ); ?>
+						<?php endif; ?>
 
-					<!-- Gets post metadata -->
+						<!-- Gets post metadata -->
 
-					<?php inhabitent_theme_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_theme_posted_by(); ?>
+						<?php inhabitent_theme_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_theme_posted_by(); ?>
 
-					<!-- get post title -->
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						<!-- get post title -->
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
-					<!-- Make Button for read more -->
-					<button><a href="<?php the_permalink(); ?>">Read more</a></button>
-				   </li>
+						<!-- Make Button for read more -->
+						<button><a href="<?php the_permalink(); ?>">Read Entry</a></button>
+					</li>
 
 				<?php endforeach; wp_reset_postdata(); ?>
 			</ul>
@@ -107,26 +106,26 @@ get_header(); ?>
 					'posts_per_page' => 4,
 					);
 				$front_page_blog_posts = get_posts($args);
-					?>
+				?>
 				
 				<?php foreach ( $front_page_blog_posts as $post ) : setup_postdata( $post ); ?>
-				   <li> 
-				  <!--  // Gets the post thumbnail MIGHT NEED TO WRAP EACH part in divs well see -->
+					<li> 
+						<!--  // Gets the post thumbnail MIGHT NEED TO WRAP EACH part in divs well see -->
 
-					 <?php if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail( 'medium' ); ?>
-					<?php endif; ?>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'medium' ); ?>
+						<?php endif; ?>
 
-					<!-- Gets post metadata -->
+						<!-- Gets post metadata -->
 
-					<?php inhabitent_theme_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_theme_posted_by(); ?>
 
-					<!-- get post title -->
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
-					<!-- Make Button for read more -->
-					<button><a href="<?php the_permalink(); ?>">Read more</a></button>
-				   </li>
+						<!-- get post title -->
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+						<!-- Make Button for read more -->
+						<button><a href="<?php the_permalink(); ?>">Read more</a></button>
+					</li>
 
 				<?php endforeach; wp_reset_postdata(); ?>
 			</ul>
@@ -140,15 +139,15 @@ get_header(); ?>
 
 
 
-			<h1>Build homepage structure here</h1>
-			<!-- PUt super custom layout Here , BELOW IS EXAMPLE POSTS-->
-			<?php while ( have_posts() ) : the_post(); ?>
+		<h1>Build homepage structure here</h1>
+		<!-- PUt super custom layout Here , BELOW IS EXAMPLE POSTS-->
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php endwhile; // End of the loop. ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
