d4cb16e04a4ce9c248d0db6bdabbaefc4bca68ec
teeppope <terraepop@gmail.com>
2016-08-24 16:41:17 -0700

    unignore my functionality plugin

---
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * POST TYPES
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+
+// Register Custom Post Type
+function inhabitent_register_product_cpt() {
+
+	$labels = array(
+		'name'                  => 'Products',
+		'singular_name'         => 'Product',
+		'menu_name'             => 'Products',
+		'name_admin_bar'        => 'product',
+		'archives'              => 'Product Archives',
+		'parent_item_colon'     => 'Parent product',
+		'all_items'             => 'All Products',
+		'add_new_item'          => 'Add newproducts',
+		'add_new'               => 'Add New Product',
+		'new_item'              => 'New Product',
+		'edit_item'             => 'Edit Product',
+		'update_item'           => 'Update Product',
+		'view_item'             => 'View Product',
+		'search_items'          => 'Search Products',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as Product image',
+		'insert_into_item'      => 'Insert into product',
+		'uploaded_to_this_item' => 'Uploaded to this product',
+		'items_list'            => 'Products list',
+		'items_list_navigation' => 'Product list navigation',
+		'filter_items_list'     => 'Filter items list',
+	);
+	$args = array(
+		'label'                 => 'Product',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'				=> 'dashicons-cart',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => 'products',
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'product', $args );
+
+}
+add_action( 'init', 'inhabitent_register_product_cpt', 0 );
+
+
+//Adventure Custom Post Type
+
+function inhabitent_register_adventure_cpt() {
+
+	$labels = array(
+		'name'                  => 'Adventures',
+		'singular_name'         => 'Adventure',
+		'menu_name'             => 'Adventure',
+		'name_admin_bar'        => 'Adventures',
+		'archives'              => 'Adventure Archives',
+		'parent_item_colon'     => 'Parent Adventure',
+		'all_items'             => 'All Adventures',
+		'add_new_item'          => 'Add New Adventure',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Adventure',
+		'edit_item'             => 'Edit Adventure',
+		'update_item'           => 'Update Adventure',
+		'view_item'             => 'ViewAdventure',
+		'search_items'          => 'Search Adventure',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into adventure',
+		'uploaded_to_this_item' => 'Uploaded to this adventure',
+		'items_list'            => 'Adventures list',
+		'items_list_navigation' => 'Adventures list navigation',
+		'filter_items_list'     => 'Filter adventures list',
+	);
+	$args = array(
+		'label'                 => 'Adventure',
+		'description'           => 'Lastest adventure posts',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-palmtree',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'adventure', $args );
+
+}
+add_action( 'init', 'inhabitent_register_adventure_cpt', 0 );
